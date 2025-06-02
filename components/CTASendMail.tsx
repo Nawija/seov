9b8c900ec61cfa87bcb26fdb7d9ed272bdfe7f40
Valkon <96625039+Nawija@users.noreply.github.com>
2025-06-03 00:51:50 +0200

    Update CTASendMail.tsx

--- components/CTASendMail.tsx
+++ components/CTASendMail.tsx
@@ -1,47 +1,47 @@
 "use client";
-import { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { MainBtn } from "./Buttons/MainBtn";
+import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
+import { MainBtn } from "./buttons/MainBtn";
 import ContactForm from "./ContactForm";
 
 export default function CTASendMail({ title }: { title: string }) {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    return (
-        <>
-            <MainBtn className="uppercase" onClick={() => setIsModalOpen(true)}>
-                {title}
-            </MainBtn>
+  return (
+    <>
+      <MainBtn className="uppercase" onClick={() => setIsModalOpen(true)}>
+        {title}
+      </MainBtn>
 
-            <AnimatePresence>
-                {isModalOpen && (
-                    <motion.div
-                        key="modal"
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 0.3 }}
-                        className="fixed -top-6 left-0 h-[110%] w-full flex items-center justify-center bg-black/50 backdrop-blur-sm z-[9999]"
-                    >
-                        <motion.div
-                            initial={{ scale: 0.95, opacity: 0 }}
-                            animate={{ scale: 1, opacity: 1 }}
-                            exit={{ scale: 0.95, opacity: 0 }}
-                            transition={{ duration: 0.3 }}
-                            className="bg-white p-6 shadow-lg max-w-lg w-[90%] -mt-20 relative"
-                        >
-                            <button
-                                className="absolute top-2 right-2"
-                                onClick={() => setIsModalOpen(false)}
-                            >
-                                <IoMdClose className="text-2xl text-black" />
-                            </button>
-                            <ContactForm />
-                        </motion.div>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </>
-    );
+      <AnimatePresence>
+        {isModalOpen && (
+          <motion.div
+            key="modal"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed -top-6 left-0 z-[9999] flex h-[110%] w-full items-center justify-center bg-black/50 backdrop-blur-sm"
+          >
+            <motion.div
+              initial={{ scale: 0.95, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.95, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+              className="relative -mt-20 w-[90%] max-w-lg bg-white p-6 shadow-lg"
+            >
+              <button
+                className="absolute top-2 right-2"
+                onClick={() => setIsModalOpen(false)}
+              >
+                <IoMdClose className="text-2xl text-black" />
+              </button>
+              <ContactForm />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
 }
